a9b17282f0ba19989d9f3203491177556cd6f720
Maksim Nikiforov <maksim.ast@gmail.com>
2021-09-25 15:08:07 -0400

    Script to render github_pages

---
+++ generateMD.R
@@ -0,0 +1,13 @@
+rmarkdown::render(
+  input="Nikiforov_NASA_API_vignette.Rmd",
+  output_format = "github_document",
+  output_file = "README.md",
+  runtime = "static",
+  clean = TRUE,
+  params = NULL,
+  knit_meta = NULL,
+  envir = parent.frame(),
+  run_pandoc = TRUE,
+  quiet = FALSE,
+  encoding = "UTF-8"
+)
